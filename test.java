130a46a5e6c818a3415ec0a3e5ffe896cff8b707
Arun Yadav <Arun Yadav@DESKTOP-P66ORNM.mshome.net>
2023-10-09 09:02:47 -0700

    programer has deed some changes from ecilips ide on test.java

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ class Test
 		        {
 				        System.out.println("My First Java Program.");
                 	    
-		                        System.out.println("This is TeamB Program. changes ");
+		            System.out.println("This is TeamB Program. changes ");
 					System.out.println("This is TeamA Program. changes ");
 					System.out.println("Now this git repository is configured with ecilips ide ");
 					
